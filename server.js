da1d5618ff953f66add1d7019e6e22ac36669af8
javyin <343713@qq.com>
2020-12-06 22:37:04 +0800

    增加备注信息

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const http = require('http').createServer(app)
 const port = 3000
 const io = require('socket.io')(http)
 
+//  这里设置浏览器访问相关路径时的操作
 app.get('/', function(req, res) {
+    // 访问'/'时发送index.html文件
     res.sendFile(__dirname + '/index.html')
 })
 
 io.on('connection', (socket)=>{
     console.log('a socket is connected')
+    // chatEvent可以监听客户端发送的消息
     socket.on('chatEvent', function(msg) {
         console.log(`${msg} from client`)
         // 用send只是让服务器回复消息
